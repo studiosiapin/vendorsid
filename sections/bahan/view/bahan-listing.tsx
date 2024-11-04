c852dacb1e50ee4ef57241b1d29a9bfcd077f24f
Adzi Bilal <adzibilal02@gmail.com>
2024-11-04 09:07:28 +0700

    Refactor BahanListingPage to remove unused search params and mock data

--- sections/bahan/view/bahan-listing.tsx
+++ sections/bahan/view/bahan-listing.tsx
@@ -1,11 +1,7 @@
-import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import { buttonVariants } from '@/components/ui/button';
 import { Heading } from '@/components/ui/heading';
 import { Separator } from '@/components/ui/separator';
-import { Bahan } from '@/constants/data';
-import { fakeBahans } from '@/constants/mock-api';
-import { searchParamsCache } from '@/lib/searchparams';
 import { cn } from '@/lib/utils';
 import { Plus } from 'lucide-react';
 import Link from 'next/link';
@@ -14,21 +10,6 @@ import BahanTable from '../bahan-tables';
 type BahanListingPage = {};
 
 export default async function BahanListingPage({}: BahanListingPage) {
-  // Showcasing the use of search params cache in nested RSCs
-  const page = searchParamsCache.get('page');
-  const search = searchParamsCache.get('q');
-  const pageLimit = searchParamsCache.get('limit');
-
-  const filters = {
-    page,
-    limit: pageLimit,
-    ...(search && { search })
-  };
-
-  const data = await fakeBahans.getData(filters);
-  const totalData = data.total_data;
-  const bahans: Bahan[] = data.data;
-
   return (
     <PageContainer>
       <div className="space-y-4">
@@ -45,7 +26,7 @@ export default async function BahanListingPage({}: BahanListingPage) {
           </Link>
         </div>
         <Separator />
-        <BahanTable data={bahans} totalData={totalData} />
+        <BahanTable />
       </div>
     </PageContainer>
   );
