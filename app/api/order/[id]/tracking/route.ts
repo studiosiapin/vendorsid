9357fd5606fb13de96bf3c763b924844e3f06f6f
Adzi Bilal <adzibilal02@gmail.com>
2024-11-23 13:11:24 +0700

    Refactor order tracking display to enhance UI and change createdAt sorting order

--- app/api/order/[id]/tracking/route.ts
+++ app/api/order/[id]/tracking/route.ts
@@ -38,7 +38,7 @@ export async function GET(
         createdAt: true
       },
       orderBy: {
-        createdAt: 'desc'
+        createdAt: 'asc'
       }
     });
 
--- sections/pemesanan/detail/index.tsx
+++ sections/pemesanan/detail/index.tsx
@@ -237,48 +237,41 @@ const DetailPageOrder = () => {
                 <div className="mt-5">
                   <div className="mb-2 font-bold">Progress</div>
 
-                  {/* Tabel Progress */}
-                  <Table className="rounded border-2">
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Updated By</TableHead>
-                        <TableHead>Time</TableHead>
-                        <TableHead>Link Progress</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                      {progress &&
-                        progress?.map((item) => (
-                          <TableRow key={item.id}>
-                            <TableCell>
+                  <div className="ml-3 mt-5 border-l-8 border-zinc-200 dark:border-zinc-800">
+                    {progress &&
+                      progress.map((item) => (
+                        <div
+                          className="relative m-3 ml-[20px] mt-8 cursor-pointer rounded-md border bg-white p-3 shadow-lg hover:bg-zinc-50 dark:bg-zinc-900 dark:hover:bg-zinc-800"
+                          key={item.id}
+                        >
+                          <div className="absolute -left-[35px] h-5 w-5 rounded-full border-2 border-white bg-blue-600"></div>
+                          <div className="absolute -top-6 left-0 text-sm text-blue-400">
+                            {formatDateTime(item.createdAt)}
+                          </div>
+                          <div className="flex items-center justify-between gap-3">
+                            <div className="flex items-center gap-3">
                               <BadgeStatus
                                 status={item.status as OrderStatus}
                               />
-                            </TableCell>
-                            <TableCell>
-                              <div className="w-max">{item.user.name}</div>
-                            </TableCell>
-                            <TableCell>
-                              <p className="min-w-max">
-                                {formatDateTime(item.createdAt)}
-                              </p>
-                            </TableCell>
-                            <TableCell>
+                              <div className="text-sm">
+                                by
+                                <span className="ml-1 text-sm font-bold">
+                                  {item.user.name}
+                                </span>
+                              </div>
+                            </div>
+
+                            <div className="">
                               {item.linkProgress && (
-                                <Link
-                                  href={item.linkProgress}
-                                  target="_blank"
-                                  className="text-blue-500 underline"
-                                >
-                                  LIHAT
+                                <Link href={item.linkProgress} target="_blank">
+                                  <Button size="sm">LIHAT</Button>
                                 </Link>
                               )}
-                            </TableCell>
-                          </TableRow>
-                        ))}
-                    </TableBody>
-                  </Table>
+                            </div>
+                          </div>
+                        </div>
+                      ))}
+                  </div>
                 </div>
 
                 <ActionButtons order={order} onUpdated={fetchAllData} />
